6a878fe497482e5acd51f6ba2847c4507592e461
lslqtz <admin@acgn.xyz>
2026-06-08 01:40:17 +0800

    Bug fix

--- ALVRClient/Entry/Entry.swift
+++ ALVRClient/Entry/Entry.swift
@@ -166,11 +166,15 @@ struct Entry: View {
                             Text("Enable Chroma Keyed Passthrough*")
                             Text("*Only works with RealityKit renderer")
                             .font(.system(size: 10))
-#endif
                         }
                         else {
                             Text("Enable Chroma Keyed Passthrough")
                         }
+#else
+                        Text("Enable Chroma Keyed Passthrough")
+#endif
+                    }
+                    .toggleStyle(.switch)
                     
                     ColorPicker("Chroma Key Color", selection: $chromaKeyColor)
                     .onChange(of: chromaKeyColor) {
